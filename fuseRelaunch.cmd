883574f583822b601d4328bb5857350b3cd01bef
Chen Mouxiang <cmx_1007@foxmail.com>
2019-01-09 22:43:57 +0800

    Update .gitignore

--- fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "G:/course/Logic and Computer Design/Project/SuperMario/test_rom_isim_beh.exe" -prj "G:/course/Logic and Computer Design/Project/SuperMario/test_rom_beh.prj" "test_rom" "work.glbl" 
